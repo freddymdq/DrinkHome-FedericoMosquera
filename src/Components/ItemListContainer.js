00ae77f025704b7c38f3fc3f8510f9f85dac88f9
Fede Mosquera <freddymdq@gmail.com>
2022-11-20 22:29:09 -0300

    Agregado href a los a

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const ItemListContainer = ({greeting, greeting2}) => {
+const ItemListContainer = ({text, text2}) => {
 
   return (
     <>
     <div className='p-4 bg-neutral text-white text-7xl '> 
-        {greeting}
+        {text}
     </div>
     <div className='p-4 bg-neutral text-white text-3xl text-success'>
-        {greeting2}
+        {text2}
     </div>
     </>
   )
